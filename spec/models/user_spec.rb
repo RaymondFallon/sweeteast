a52f128cae22ef9bcb33e209c18bb9f557ef7624
Raymond Fallon <raymond.fallon@pennmedicine.upenn.edu>
2024-04-13 18:14:43 -0400

    Link to Showtime#index on homepage, add author names to homepage

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'is valid with an email and password' do
+    user = User.new(
+      email: 'ray@example.com',
+      password: 'password'
+    )
+    expect(user).to be_valid
+  end
 end
