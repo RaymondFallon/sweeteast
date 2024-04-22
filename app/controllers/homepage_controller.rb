f977c2e823637a5abdebba7239aa5b197a8024c6
Raymond Fallon <raymond.fallon@pennmedicine.upenn.edu>
2024-04-22 14:33:15 -0400

    Show more article previews

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,7 @@
 
 class HomepageController < ApplicationController
   def home
-    @articles = Article.published.order(published_at: :desc).limit(5)
+    @articles = Article.published.order(published_at: :desc).limit(8)
     @movies_today = Movie.for_date(Time.zone.today)
     @movies_later = Movie.this_week - @movies_today
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,21 +2,24 @@
 ## Theaters
 
 [['pgt', 'Princeton Garden Theatre', 'https://www.princetongardentheatre.org/',
-          '160 Nassau St, Princeton, NJ 08542', '08542'],
+         '160 Nassau St, Princeton, NJ 08542', '08542'],
  ['delsea', 'Delsea Drive-In', 'https://www.delseadrive-in.com/',
             '2203 S Delsea Dr, Vineland, NJ 08360', '08360'],
  ['smodcastle', 'Smodcastle Cinemas', 'https://www.smodcastlecinemas.com/',
-         '123 Main St, Red Bank, NJ 07701', '07701'],
+                '123 Main St, Red Bank, NJ 07701', '07701'],
  ['barrymore', 'Barrymore Film Center', 'https://www.barrymorefilmcenter.com/',
-         '209 Monmouth St, Red Bank, NJ 07701', '07701'],
+               '209 Monmouth St, Red Bank, NJ 07701', '07701'],
  ['clairidge', 'Clairidge', 'https://theclairidge.org/',
-         '486 Bloomfield Ave, Montclair, NJ 07042', '07042'],
+               '486 Bloomfield Ave, Montclair, NJ 07042', '07042'],
  ['showroom', 'Showroom Cinemas', 'https://showroomcinemas.com/',
-         '707 Cookman Ave, Asbury Park, NJ 07712', '07712'],
+              '707 Cookman Ave, Asbury Park, NJ 07712', '07712'],
  ['williams', 'Williams Center', 'https://www.williamscenter.co/cinema-underground',
-         '1 Williams Plaza, Rutherford, NJ 07070', '07070'],
+              '1 Williams Plaza, Rutherford, NJ 07070', '07070'],
  ['montgomery', 'Montgomery Cinemas', 'https://www.montgomerycinemas.com/',
-         '1325 US-206, Skillman, NJ 08558', '08558']].each do |code, name, url, address, zip_code|
+                '1325 US-206, Skillman, NJ 08558', '08558'],
+ ['newark_lgbtq', 'Newark LGBTQ Center', 'https://www.newarklgbtqcenter.org/',
+                  '89 Market St, Newark, NJ 07102', '07102'],
+].each do |code, name, url, address, zip_code|
   Theater.where(code: code).first_or_create(name: name, external_url: url, address: address, zip_code: zip_code)
 end
 
