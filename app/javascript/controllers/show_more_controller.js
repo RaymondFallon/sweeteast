925f28bb1168faa2e7cab5cd27b4ef0e311e3225
Raymond Fallon <raymond.fallon@pennmedicine.upenn.edu>
2024-05-17 10:22:24 -0400

    Make show-more controller a progressive enhancement, aka work on my oldass phone

--- app/javascript/controllers/show_more_controller.js
+++ app/javascript/controllers/show_more_controller.js
@@ -3,6 +3,15 @@ import { Controller } from "@hotwired/stimulus"
 export default class extends Controller {
   static targets = ['content', 'control']
 
+  connect() {
+    this.showLess();
+  }
+
+  showLess() {
+    this.contentTarget.classList.add('shrunken')
+    this.controlTarget.classList.remove('hidden')
+  }
+
   showMore() {
     this.contentTarget.classList.remove('shrunken')
     this.controlTarget.classList.add('hidden')
